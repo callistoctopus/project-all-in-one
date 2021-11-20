2e885f3f6b78d77cb6fab9d06dbcf86fdbff6199
callistoctopus <1101446261@qq.com>
2021-11-20 22:15:32 +0800

    update project

--- server/webservice/spring-boot/spring-boot-web/mvc/project/src/main/java/com/callistoctopus/springbootweb/controller/ControllerPrototype.java
+++ server/webservice/spring-boot/spring-boot-web/mvc/project/src/main/java/com/callistoctopus/springbootweb/controller/ControllerPrototype.java
@@ -1,19 +1,74 @@
 package com.callistoctopus.springbootweb.controller;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+import java.util.UUID;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
+@Api(value = "desc of class")
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value="/king")
 public class ControllerPrototype {
 
-    @RequestMapping(value="/{user}", method=RequestMethod.GET)
-    public User getUser(@PathVariable Long user) {
-        return new User("name-test", "sex-test");
+    // private List<Pic> picList = new ArrayList<Pic>();
+    Stack<Pic> picList = new Stack<Pic>();
+
+    synchronized void push(Pic pic){
+        picList.push(pic);
+    }
+
+    synchronized Pic pop(){
+        return picList.pop();
+    }
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public boolean getUser(@RequestBody Pic pic) {
+
+        // String picData = pic.getData();
+        // picData = picData.substring(30);
+        // try {
+        //     picData = URLDecoder.decode(picData,"UTF-8");
+        // } catch (UnsupportedEncodingException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+
+        // String photoName = String.valueOf(UUID.randomUUID()) + ".png";
+        // String imgUrl = "/var/work/project/data/" + photoName;
+        // boolean res= GenerateImage(picData,imgUrl);
+        push(pic);
+
+        return true;
+    }
+
+    @RequestMapping(value="/download", method=RequestMethod.POST)
+    public Pic download() {
+        // picData = picData.substring(30);
+        // try {
+        //     picData = URLDecoder.decode(picData,"UTF-8");
+        // } catch (UnsupportedEncodingException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+
+        // String photoName = String.valueOf(UUID.randomUUID()) + ".png";
+        // String imgUrl = "/var/work/project/data/" + photoName;
+        // boolean res= GenerateImage(picData,imgUrl);
+
+        return pop();
     }
 
     @RequestMapping(value="/{user}/customers", method=RequestMethod.GET)
@@ -25,6 +80,36 @@ public class ControllerPrototype {
     @RequestMapping(value="/{user}", method=RequestMethod.DELETE)
     public User deleteUser(@PathVariable Long user) {
         return new User("name-test", "sex-test");
+    }
+
+    public static  boolean  GenerateImage(String imgStr,String url){
+ 
+        if (imgStr == null) return false;
+        // BASE64Decoder decoder = new BASE64Decoder();
+        
+        try
+        {
+            //Base64解码
+            // byte[] b = decoder.decodeBuffer(imgStr);
+            byte[] b = Base64.decodeBase64(imgStr);
+            for(int i=0;i<b.length;++i)
+            {
+                if(b[i]<0)
+                {//调整异常数据
+                    b[i]+=256;
+                }
+            }
+            //生成jpeg图片
+            OutputStream out = new FileOutputStream(url);
+            out.write(b);
+            out.flush();
+            out.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 
 }
@@ -48,6 +133,28 @@ class User {
     }
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    //setter,getter...
+}
+
+class Pic {
+    
+    private String data;
+
+    public Pic() {
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public Pic(String data) {
+        this.data = data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 
     //setter,getter...
