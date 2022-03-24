8fe5f2ff2de3cf92cfe827c475a63e8b119080f7
callistoctopus <1101446261@qq.com>
2022-03-24 16:10:57 +0800

    add

---
+++ project-in-action/iot-data-view/src/db/ddl.sql
@@ -0,0 +1,7 @@
+-- iot.hero definition
+
+CREATE TABLE `hero` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
