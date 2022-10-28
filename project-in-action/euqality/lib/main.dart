9351e5cbd535093e8966679a58cef9dba37961a8
callistoctopus <1101446261@qq.com>
2022-10-28 01:28:18 +0000

    add

---
+++ project-in-action/euqality/lib/main.dart
@@ -0,0 +1,76 @@
+/*
+ * @Author: gui-qi
+ * @Date: 2022-10-26 08:18:13
+ * @LastEditors: gui-qi
+ * @LastEditTime: 2022-10-27 08:42:49
+ * @Description: 
+ * 
+ * Copyright (c) 2022, All Rights Reserved. 
+ */
+
+// flutter pub add english_words
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  var _wordPair = WordPair.random();
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _wordPair = WordPair.random();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*appBar: AppBar(
+        title: Text(widget.title),
+      ),*/
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_wordPair',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
