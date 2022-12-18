b816ef380c849cd462616348b3106cf991526a10
callistoctopus <1101446261@qq.com>
2022-12-18 12:33:16 +0800

    u

--- project-in-action/common-web-service/src/data-api/src/main/java/com/callistoctopus/springbootweb/controller/ControllerPrototype.java
+++ project-in-action/common-web-service/src/data-api/src/main/java/com/callistoctopus/springbootweb/controller/ControllerPrototype.java
@@ -9,8 +9,10 @@ import java.util.List;
 import java.util.Stack;
 import java.util.UUID;
 
-import com.callistoctopus.springbootweb.dao.HeroDao;
+import com.alibaba.fastjson.JSONObject;
+// import com.callistoctopus.springbootweb.dao.HeroDao;
 import com.callistoctopus.springbootweb.model.po.Hero;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
+// import io.swagger.annotations.Api;
 
-@Api(value = "desc of class")
+// @Api(value = "desc of class")
 @RestController
 @RequestMapping(value="/api")
 public class ControllerPrototype {
@@ -30,8 +32,8 @@ public class ControllerPrototype {
     // private List<Pic> picList = new ArrayList<Pic>();
     Stack<Pic> picList = new Stack<Pic>();
 
-    @Autowired
-    HeroDao heroDao;
+    // @Autowired
+    // HeroDao heroDao;
 
     synchronized void push(Pic pic){
         picList.push(pic);
@@ -41,17 +43,18 @@ public class ControllerPrototype {
         return picList.pop();
     }
 
-    @RequestMapping(value="/heroes", method=RequestMethod.GET)
-    List<Hero> getCustomers() throws Exception {
-        List<Hero> list = new ArrayList<>();
-        Iterable<Hero> geted = heroDao.findAll();
-        geted.forEach(single -> {list.add(single);});
-        return list;
-    }
+    // @RequestMapping(value="/heroes", method=RequestMethod.GET)
+    // List<Hero> getCustomers() throws Exception {
+    //     List<Hero> list = new ArrayList<>();
+    //     Iterable<Hero> geted = heroDao.findAll();
+    //     geted.forEach(single -> {list.add(single);});
+    //     return list;
+    // }
 
-    @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public boolean getUser(@RequestBody Pic pic) {
+    @RequestMapping(value="/outer_system/getDataListOfElectronicReport", method=RequestMethod.POST)
+    public JSONObject getUser() {
 
+        String resStr = "{\"success\":true,\"code\":\"200\",\"timestamp\":1662717276517,\"message\":{\"messageNo\":\"MSG-00001\",\"messageInfo\":\"处理正常。\",\"detaileInfo\":\"\"},\"result\":{\"pageNo\":1,\"pageSize\":2,\"total\":2,\"isFirstPage\":true,\"isLastPage\":true,\"pages\":1,\"dataList\":[{\"shipOwner\":\"徐伟\",\"updateDatetime\":\"2022-09-1510:10:00\",\"applyPersonTel\":\"13456822179\",\"shipName\":\"浙安吉货1361\",\"applyPerson\":\"徐伟\",\"deleteFlag\":\"0\",\"createDatetime\":\"2022-09-1510:10:00\",\"uid\":\"990000001\",\"boundType\":\"进港\",\"registPort\":\"湖州\",\"berth\":\"湖州市菱城建设工程有限公司\",\"reportDatetime\":\"2022-05-0115:29:00\",\"reportNo\":\"CBBG202204941788\"},{\"shipOwner\":\"王小明\",\"updateDatetime\":\"2022-09-0706:14:00\",\"applyPersonTel\":\"13967299156\",\"shipName\":\"浙湖州货2955\",\"applyPerson\":\"朱兰海\",\"deleteFlag\":\"0\",\"createDatetime\":\"2022-09-0706:14:00\",\"uid\":\"990000002\",\"boundType\":\"出港\",\"registPort\":\"湖州\",\"berth\":\"湖州鹿山坞建材科技有限公司码头\",\"reportDatetime\":\"2022-05-0115:22:00\",\"reportNo\":\"CBBG202204941480\"}]}}";
         // String picData = pic.getData();
         // picData = picData.substring(30);
         // try {
@@ -64,9 +67,9 @@ public class ControllerPrototype {
         // String photoName = String.valueOf(UUID.randomUUID()) + ".png";
         // String imgUrl = "/var/work/project/data/" + photoName;
         // boolean res= GenerateImage(picData,imgUrl);
-        push(pic);
+        // push(pic);
 
-        return true;
+        return JSONObject.parseObject(resStr);
     }
 
     @RequestMapping(value="/download", method=RequestMethod.POST)
--- project-in-action/common-web-service/src/data-api/src/main/java/com/callistoctopus/springbootweb/dao/HeroDao.java
+++ project-in-action/common-web-service/src/data-api/src/main/java/com/callistoctopus/springbootweb/dao/HeroDao.java
@@ -1,25 +1,34 @@
-package com.callistoctopus.springbootweb.dao;
+// /*
+//  * @Author: gui-qi
+//  * @Date: 2022-09-21 21:09:38
+//  * @LastEditors: gui-qi
+//  * @LastEditTime: 2022-09-21 22:16:37
+//  * @Description: 
+//  * 
+//  * Copyright (c) 2022, All Rights Reserved. 
+//  */
+// package com.callistoctopus.springbootweb.dao;
 
-import java.util.List;
+// import java.util.List;
 
-import com.callistoctopus.springbootweb.model.po.Hero;
+// import com.callistoctopus.springbootweb.model.po.Hero;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.repository.CrudRepository;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Repository;
 
-@Repository
-public interface HeroDao extends CrudRepository<Hero, Long>{
-    // private final JdbcTemplate jdbcTemplate;
+// // @Repository
+// // public interface HeroDao extends CrudRepository<Hero, Long>{
+// //     // private final JdbcTemplate jdbcTemplate;
 
-    // @Autowired
-    // public HeroDao(JdbcTemplate jdbcTemplate) {
-    //     this.jdbcTemplate = jdbcTemplate;
-    // }
+// //     // @Autowired
+// //     // public HeroDao(JdbcTemplate jdbcTemplate) {
+// //     //     this.jdbcTemplate = jdbcTemplate;
+// //     // }
 
-    // public List<Hero> getHeroes(){
-    //     return jdbcTemplate.queryForList("select * from hero", Hero.class);
-    // }
-}
+// //     // public List<Hero> getHeroes(){
+// //     //     return jdbcTemplate.queryForList("select * from hero", Hero.class);
+// //     // }
+// // }
